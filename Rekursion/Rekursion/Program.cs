dc6fe435f3f273dbcf60c7549b48343cc30000cb
Thomas Thorin <thomas.thorin@gmail.com>
2022-06-16 15:54:13 +0200

    stuff

---
+++ Rekursion/Rekursion/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static Rekursion.Recursion;
+
+var wantToKnow = 5;
+Console.WriteLine($"Factorial of {wantToKnow} is {Factorial(wantToKnow)}.");
+
+PrintNumbers(5);
+
+var startAmount = 100;
+Console.WriteLine(CalculateBalance(startAmount,50));
+
+int[] myArr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var result = ArraySum(myArr, 8);
+Console.WriteLine($"ArraySum result = {result}");
+
+var whatIsFibFor = 400;
+//Console.WriteLine($"Number {whatIsFibFor} in the Fibonacci sequence is {Fibonacci(whatIsFibFor)}.");
+//Console.WriteLine($"(Iterative)Number {whatIsFibFor} in the Fibonacci sequence is {FibIterative(whatIsFibFor)}.");
+//for (int i = 0; i <= whatIsFibFor; i++)
+//{
+//    var loopResult = FibIterative(i);
+//    if (loopResult < 0)
+//    {
+//        Console.WriteLine($"i is {i}, value caused overflow, value is {loopResult}");
+//        break;
+//    }
+//    Console.WriteLine(loopResult);
+//}
+
+//PrintEven(0, 10);
+Console.WriteLine(Reverse("hejhej"));
+Console.WriteLine("is pap papp a palindrome? "+IsPalindrome("pap papp"));
+Console.WriteLine("is ni talar bra latin a palindrome? " + IsPalindrome("ni talar bra latin"));
---
+++ Rekursion/Rekursion/Recursion.cs
@@ -0,0 +1,69 @@
+﻿namespace Rekursion
+{
+    internal static class Recursion
+    {
+        public static int Factorial(int n)
+        {
+            if (n <= 0) return 1;
+            return n * Factorial(n - 1);
+        }
+
+        public static void PrintNumbers(int num)
+        {
+            if (num < 1) return;
+            PrintNumbers(num - 1);
+            Console.WriteLine(num);
+        }
+        public static double CalculateBalance(double balance, int years)
+        {
+            const double interest = 0.05;
+            if (years == 1) return balance + balance * interest;
+
+            var accumulated = CalculateBalance(balance, years - 1);
+            return accumulated + accumulated * interest;
+        }
+        public static int ArraySum(int[] array, int startIndex = 0)
+        {
+            if (startIndex == array.Length - 1) return array[startIndex];
+            return array[startIndex] + ArraySum(array, startIndex + 1);
+        }
+        public static int Fibonacci(int n)
+        {
+            if (n == 1) return 0;
+            else if (n == 2) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        public static long FibIterative(int n)
+        {
+            if (n <= 1) return 0;
+            if (n == 2) return 1;
+            //var fibList = new List<long> { 0, 1 };
+            long twoBack = 0;
+            long oneBack = 1;
+            long answer = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                answer = oneBack + twoBack;
+                twoBack = oneBack;
+                oneBack = answer;
+            }
+            return answer;
+        }
+        public static void PrintEven(int min, int max)
+        {
+            if (max < min) return;
+            PrintEven(min, max - 1);
+            if (max % 2 == 0) Console.WriteLine(max);
+        }
+        public static string Reverse(string s, int startAt = 0)
+        {
+            if (startAt == s.Length - 1) return s[startAt].ToString();
+            return Reverse(s, startAt + 1) + s[startAt].ToString();
+        }
+        
+        public static bool IsPalindrome(string s)
+        {
+            return s.Replace(" ","").Equals(Reverse(s).Replace(" ",""));
+        }
+    }
+}
